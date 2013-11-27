eb01c54837c55c736ca53fa462e3bf8d49edcf1a
David Baumgold <david@davidbaumgold.com>
2013-11-27 15:33:57 -0500

    fix up patches and build process

--- litecoind.rb
+++ litecoind.rb
@@ -24,26 +24,17 @@ class Litecoind < Formula
   option 'without-ipv6', 'Compile without IPv6 support'
 
   def patches
-    unless build.head?
-      # fix include and lib paths for berkeley-db4 and openssl
-      DATA
-    end
+    'contrib/homebrew/makefile.osx.patch'
   end
 
   def install
     raise 'Litecoind currently requires --HEAD on Mavericks' if MacOS.version == :mavericks and not build.head?
 
-    if build.head?
-      system "sh", "autogen.sh"
-      system "./configure", "--prefix=#{prefix}"
-      system "make"
-    else
-      cd "src" do
-        system "make", "-f", "makefile.osx",
-          "DEPSDIR=#{HOMEBREW_PREFIX}",
-          "USE_UPNP=#{(build.include? 'with-upnp') ? '1' : '-'}",
-          "USE_IPV6=#{(build.include? 'without-ipv6') ? '-' : '1'}"
-      end
+    cd "src" do
+      system "make", "-f", "makefile.osx",
+        "DEPSDIR=#{HOMEBREW_PREFIX}",
+        "USE_UPNP=#{(build.include? 'with-upnp') ? '1' : '-'}",
+        "USE_IPV6=#{(build.include? 'without-ipv6') ? '-' : '1'}"
     end
 
     system "strip src/litecoind"
@@ -81,52 +72,4 @@ class Litecoind < Formula
     EOS
   end
 end
-__END__
-diff --git a/src/makefile.osx b/src/makefile.osx
-index 50279fd..6ab92b1 100644
---- a/src/makefile.osx
-+++ b/src/makefile.osx
-@@ -7,18 +7,22 @@
- # Originally by Laszlo Hanyecz (solar@heliacal.net)
- 
- CXX=llvm-g++
- DEPSDIR=/opt/local
-+DB4DIR=$(DEPSDIR)/opt/berkeley-db4
-+OPENSSLDIR=$(DEPSDIR)/opt/openssl
- 
- INCLUDEPATHS= \
-  -I"$(CURDIR)" \
-- -I"$(CURDIR)"/obj \
-+ -I"$(CURDIR)/obj" \
-  -I"$(DEPSDIR)/include" \
-- -I"$(DEPSDIR)/include/db48"
-+ -I"$(DB4DIR)/include" \
-+ -I"$(OPENSSLDIR)/include"
- 
- LIBPATHS= \
-  -L"$(DEPSDIR)/lib" \
-- -L"$(DEPSDIR)/lib/db48"
-+ -L"$(DB4DIR)/lib" \
-+ -L"$(OPENSSLDIR)/lib"
- 
- USE_UPNP:=1
- USE_IPV6:=1
- 
-@@ -31,13 +35,13 @@ ifdef STATIC
- TESTLIBS += \
-  $(DEPSDIR)/lib/libboost_unit_test_framework-mt.a
- LIBS += \
-- $(DEPSDIR)/lib/db48/libdb_cxx-4.8.a \
-+ $(DB4DIR)/lib/libdb_cxx-4.8.a \
-  $(DEPSDIR)/lib/libboost_system-mt.a \
-  $(DEPSDIR)/lib/libboost_filesystem-mt.a \
-  $(DEPSDIR)/lib/libboost_program_options-mt.a \
-  $(DEPSDIR)/lib/libboost_thread-mt.a \
-  $(DEPSDIR)/lib/libboost_chrono-mt.a \
-- $(DEPSDIR)/lib/libssl.a \
-- $(DEPSDIR)/lib/libcrypto.a \
-+ $(OPENSSLDIR)/lib/libssl.a \
-+ $(OPENSSLDIR)/lib/libcrypto.a \
-  -lz
- else
- TESTLIBS += \
+
